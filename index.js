45a354451c0da2b45a943f0b71bc857275970e93
Rohan Lakhani <rohankumar.lakhani@zuru.com>
2024-12-17 12:51:30 +0530

    remove unused package

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
-import express from 'express';
-import dotenv from 'dotenv';
-import bodyParser from 'body-parser';
+import express from "express";
+import dotenv from "dotenv";
 import userRoute from "./routes/userRoute.js";
 
 dotenv.config();
@@ -9,13 +8,11 @@ const app = express();
 
 // MIDDLEWARE
 app.use(express.json());
-app.use(bodyParser.json());
-
 
 app.use("/", userRoute);
 
 const PORT = process.env.PORT || 4000;
 
 app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`);
+  console.log(`Server is running on port ${PORT}`);
 });
